7529f741817939765d5b49c4478465ee32f6318c
sophyyang <sophy_cheng@hotmail.com>
2016-01-23 00:39:17 -0600

    checkin 1/23/2016

--- LadyBugTracker/src/Tables/LadyBugData.java
+++ LadyBugTracker/src/Tables/LadyBugData.java
@@ -11,6 +11,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,12 +19,44 @@ public class LadyBugData {
 	Connection con = null;
 	Statement st = null;
 	ResultSet rs = null;
+	ArrayList<user> arrayList = new ArrayList<user>();
 
-	public LadyBugData() {
+ 	
+	public LadyBugData( ) throws SQLException {
+		
+//		try {
+			makeConnection();
+			String q = "select * from user";
+			st = con.createStatement();
+			rs = st.executeQuery(q);
+			System.out.println("start....");
+			while(rs.next()) {
+				Tables.user u = new Tables.user();
+				u.setUserID(rs.getInt(1));
+				u.setFirstName(rs.getString(2));
+				u.setLastName(rs.getString(3));
+				u.seteMailAdd(rs.getString(4));
+				u.setRoleID(rs.getInt(5));
+				u.setCreatedDate(rs.getDate(6));
+				u.setLastModified(rs.getDate(7));				
+				arrayList.add(u);				
+			}
+			System.out.println(arrayList);
 
-	}
-	
-	public LadyBugData(String tableName) {
+			if (rs != null) {
+				rs.close();
+			}
+			if (st != null) {
+				st.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		
+//		} catch (SQLException e) {
+//			System.out.println("Error with table or data");
+//		}
+		
 		
 	}
 	

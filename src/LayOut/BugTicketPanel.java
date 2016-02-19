d66e355bf852e84ada7298b00b959b43aa4a248d
Mary Larsen <mlarsen3@dmacc.edu>
2016-02-19 13:03:13 -0600

    Updated the BugTicketPanel - it works now!

--- src/LayOut/BugTicketPanel.java
+++ src/LayOut/BugTicketPanel.java
@@ -2,52 +2,54 @@ package LayOut;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.ComboBoxModel;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-// MARY LARSEN //
+import Tables.LadyBugData;
+
+// MARY LARSEN 
 public class BugTicketPanel extends JPanel {
-
-	Container c = getRootPane();
-	Tables.LadyBugData lbdata = new Tables.LadyBugData();	 
 	
- 	//* UNABLE TO GET TABLE DATA INTO DROPDOWNS; HARD CODED BELOW FOR NOW
-	String[] PriorityID = { "High", "Middle", "Low" };
-	String[] UserID = { "MLarsen3@dmacc.edu", "Test@dmacc.edu" };
+	private final int statusNo = 1;
+	private final int roleNo = 2;
+	private final int priorityNo = 3;
+	private LadyBugData rsList = new LadyBugData();
+	
+ 	//* STILL NEED TO GET TABLE DATA INTO DROPDOWNS; HARD CODED BELOW FOR NOW
 	String TicketNo = "1";
-	String[] StatusID = { "New", "In Progress", "Reassign", "Closed" };
-
+	//String[] UserID = { "MLarsen3@dmacc.edu", "Test@dmacc.edu" };
+	//String[] priorityNo = { "High", "Middle", "Low" };
+	//String[] statusNo = { "New", "In Progress", "Reassign", "Closed" };
+	
 	JLabel ticketNoLabel = new JLabel("Ticket No.");
 	JLabel ticketNo = new JLabel(TicketNo);
+	
 	JLabel userIdLabel = new JLabel("User ID");
-	JComboBox userId = new JComboBox(UserID);
+	JComboBox userId = new JComboBox();
+	
 	JLabel priorityLabel = new JLabel("Priority");
-	JComboBox priority = new JComboBox(PriorityID);
+	JComboBox priority = new JComboBox();
+	
 	JLabel statusLabel = new JLabel("Status");
-	JComboBox status = new JComboBox(StatusID);
+	JComboBox status = new JComboBox();
+	
 	JLabel titleLabel = new JLabel("Title");
 	JTextField title = new JTextField(45);
+	
 	JLabel descLabel = new JLabel("Description");
 	JTextField desc = new JTextField(50);
 
+	
 	JButton admin = new JButton("Admin");
 	JButton report = new JButton("Report");
 	JButton submit = new JButton("Submit");
@@ -55,7 +57,12 @@ public class BugTicketPanel extends JPanel {
 
 	
 	public BugTicketPanel() {
-
+		//change this one later to userid and user after Sophys changes are in
+		userId = new JComboBox(rsList.buildDropDownArray(statusNo));
+		//itemsDropDown = new JComboBox(rsList.buildUserDropDownArray(roleNo));
+		status = new JComboBox(rsList.buildDropDownArray(statusNo));
+		priority = new JComboBox(rsList.buildDropDownArray(priorityNo));
+		
 		ButtonListener b = new ButtonListener();
 		admin.addActionListener(b);
 		report.addActionListener(b);
@@ -104,7 +111,7 @@ public class BugTicketPanel extends JPanel {
 
 			if (e.getSource() == admin) {
 				removeAll();
-				JPanel newPanel = new MainPanel(); 	//* CHANGES TO ADMIN PANEL
+				JPanel newPanel = new JTabbedPanel(); 	 
 				add(newPanel);
 				revalidate();
 				newPanel.repaint();
@@ -127,7 +134,7 @@ public class BugTicketPanel extends JPanel {
 
 			if (e.getSource() == logout) {
 				removeAll();
-				JPanel newPanel = new MainPanel();
+				JPanel newPanel = new LogInPanel();
 				add(newPanel);
 				revalidate();
 				newPanel.repaint();
